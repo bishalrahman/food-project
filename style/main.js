ad3a1081b72d6f212aef297b613c765f51865a8d
bishalrahman <96377148+bishalrahman@users.noreply.github.com>
2022-01-29 09:54:36 +0600

    Add files via upload

---
+++ style/main.js
@@ -0,0 +1,53 @@
+
+$(".mobile-menu").click(function(){
+  $(".menu-item").slideToggle(300)
+});
+
+
+let nav = document.querySelector("nav")
+window.onscroll=()=>{
+   if(window.scrollY>=100){
+       nav.classList.add("active")
+   }
+   else{
+        nav.classList.remove("active")
+    }
+}
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+
+
+
+
+
+
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 22, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
